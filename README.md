46dd13d801b05b2c92b9e8d394956e17f094a53a
Guto Foletto <gutofoletto@gmail.com>
2015-09-02 15:37:31 -0300

    first commit
    main files and structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sphere Canvas2D
+
+A  study done in c++ for a game development subject with the objective to learn how to create a 3d sphere.
---
+++ src/main.cpp
@@ -0,0 +1,73 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "gl_canvas2d.h"
+
+const int w = 800;
+const int h = 600;
+
+
+int main(void) {
+    initCanvas(w, h);
+    runCanvas();
+}
+
+//funcao chamada continuamente. Deve-se controlar o que desenhar por meio de variaveis
+//globais que podem ser setadas pelo metodo keyboard()
+void render()
+{
+
+}
+
+//funcao chamada toda vez que uma tecla for pressionada
+void keyboard(int key)
+{
+    switch(key) {
+        case 27:
+            //esc
+            exit(0);
+            break;
+        case 32:
+            //spacebar
+            break;
+        case 200:
+            //left
+            break;
+        case 201:
+            //up
+            break;
+        case 202:
+            //right
+            break;
+        case 203:
+            //down
+            break;
+    }
+}
+
+//funcao chamada toda vez que uma tecla for liberada
+void keyboardUp(int key) {
+    switch(key) {
+        case 32:
+            //spacebar
+            break;
+        case 200:
+            //left
+            break;
+        case 201:
+            //up
+            break;
+        case 202:
+            //right
+            break;
+        case 203:
+            //down
+            break;
+    }
+}
+
+//funcao para tratamento de mouse: cliques, movimentos e arrastos
+void mouse(int button, int state, int x, int y) {
+    //printf("\nmouse %d %d %d %d", button, state, x, y);
+}
